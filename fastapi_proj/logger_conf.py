5516818244a589f23e7ca2abffc25a732e5c08e8
ilya-4real <ilya.chernishyov@gmail.com>
2024-06-25 00:58:45 +0200

    fix: added MDDE to settings and added DEV logging config depending on MODE setting

--- fastapi_proj/logger_conf.py
+++ fastapi_proj/logger_conf.py
@@ -1,14 +1,21 @@
 import json
 import logging
 import logging.config
-import logging.handlers
+
+from fastapi_proj.config import settings
 
 logger = logging.getLogger(__name__)
 
 
 def configure_logger():
     logger_config = None
-    with open("fastapi_proj/logger_config.json") as json_conf:
+    config_file = (
+        "fastapi_proj/logger_config_dev.json"
+        if settings.MODE == "DEV"
+        else "fastapi_proj/logger_config.json"
+    )
+    print(config_file)
+    with open(config_file) as json_conf:
         logger_config = json.load(json_conf)
 
     logging.config.dictConfig(config=logger_config)
